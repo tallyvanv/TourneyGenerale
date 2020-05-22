c61fc9c5365beaa9cb43c6aaf0f6d41d93af0b6b
tallyvanv <talisa.vdv@gmail.com>
2020-05-22 15:18:42 +0200

    cosmetic changes

--- src/Controller/TeamHomepageController.php
+++ src/Controller/TeamHomepageController.php
@@ -6,24 +6,18 @@ use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\User\UserInterface;
 
 class TeamHomepageController extends AbstractController
 {
     /**
      * @Route("/team/homepage", name="team_homepage")
-     * @param UserInterface $user
      * @return Response
      */
-    public function index(UserInterface $user)
+    public function index()
     {
         $users = $this->getDoctrine()->getRepository(User::class)->findBy(
             ['team' => null]
         );
-
-        $loggedInUser = $this->getUser();
-
-
 
         return $this->render('team_homepage/index.html.twig', [
             'controller_name' => 'TeamHomepageController',
